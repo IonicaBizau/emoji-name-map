61b8bf949aec1576e30ed5ca5940a8d8d3c24c96
Ionică Bizău <bizauionica@gmail.com>
2016-02-07 17:15:05 +0200

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,20 @@
+const name = require("../lib");
+
+console.log(name.get("heart_eyes"));
+// 😍
+
+console.log(name.emoji);
+// {
+//   '100': '💯',
+//   '1234': '🔢',
+//   grinning: '😀',
+//   grimacing: '😬',
+//   grin: '😁',
+//   joy: '😂',
+//   smiley: '😃',
+//   smile: '😄',
+//   sweat_smile: '😅',
+//   laughing: '😆',
+//   innocent: '😇',
+//   ...
+// }
